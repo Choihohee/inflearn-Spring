cc5477f60597f50dd94be15efb6f786306cac10e
Choihohee <lovelove4927@naver.com>
2023-10-29 08:50:18 +0900

    first commit

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
-
-	Hello hello = new Hello();
-	//String data = hello.setData("hello");
-	String data = hello.getData();
-	//System.out.println("data = " + data);
-
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
