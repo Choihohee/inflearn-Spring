2a7afd7c4bd4a790afa52b0534f12b16749720f7
Choihohee <lovelove4927@naver.com>
2024-01-01 23:11:21 +0900

    refactor: MemberRepository

--- jpashop/src/main/java/jpabook/jpashop/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -7,10 +7,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter@Setter
+@Getter @Setter
 public class Member {
     @Id @GeneratedValue//식별자를 Id 에 매핑, 제너레이트로 자동생성되게 만듦
     private Long id;
     private String username;
-
 }
--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,31 +1,36 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
-    @Autowired
-    MemberRepository memberRepository;
+
+    @Autowired MemberRepository memberRepository;
 
     @Test
     @Transactional
-    @Rollback(false)
-    public void testMember() {
+    public void testMember() throws Exception{
+        //given
         Member member = new Member();
         member.setUsername("memberA");
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find((savedId));
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
 
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
     }
+
 }
